f42b27ffd2a8bffae32c6ebf0b71e0895d64016f
Jahid Eljah <jahideljah@gmail.com>
2023-01-02 18:05:00 +0100

    some changes

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,30 +1,56 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
-const baseURL = "https://jsonplaceholder.typicode.com/users/1";
+const baseURL = "https://jsonplaceholder.typicode.com/users";
+// const baseURL = "https://jsonplaceholder.typicode.com/users/1";
 
 function Users() {
   const [users, setUsers] = useState([]);
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    /* axios.get("https://jsonplaceholder.typicode.com/users").then((response) => {
-      setUsers(response.data);
-    }); */
+    /////////////////////////////
     //// Mosh ////
+    /////////////////////////////
     /*  async function getUsers() {
       const re = await axios("https://jsonplaceholder.typicode.com/users");
       setUsers(re.data);
+      console.log(re.data);
     }
 
     getUsers(); */
+
+    /////////////////////////////
     /////////////////////////////
 
-    /* axios.get(baseURL).then((response) => {
-      setPost(response.data);
+    /*  const options = {
+      method: "GET",
+      url: "https://jsonplaceholder30.p.rapidapi.com/users",
+      headers: {
+        "X-RapidAPI-Key": "2e69d7ab27msh2b1ed5f886aadeep12ef8cjsnd2412a7fd097",
+        "X-RapidAPI-Host": "jsonplaceholder30.p.rapidapi.com",
+      },
+    };
+
+    axios.request(options).then(function (response) {
+      setUsers(response.data);
+      console.log(response.data);
     }); */
     /////////////////////////////
+    /////////////////////////////
 
+    axios.request(baseURL).then(function (res) {
+      // request => get
+      setUsers(res.data);
+      console.log(res.data);
+    });
+    /////////////////////////////
+    /////////////////////////////
+    /* fetch(baseURL)
+      .then((res) => res.json())
+      .then((data) => setUsers(data)); */
+    /////////////////////////////
+    /////////////////////////////
     /* fetch(baseURL)
       .then((res) => {
         if (!res.ok) {
@@ -32,31 +58,23 @@ function Users() {
         }
         return res.json();
       })
-      .then((data) => setPosts(data)); */
-
-    fetch(baseURL)
-      .then((res) => {
-        if (!res.ok) {
-          return Error("Oh no");
-        }
-        return res.json();
-      })
-      .then((data) => setUsers(data));
+      .then((data) => setUsers(data)); */
+    /////////////////////////////
   }, []);
 
   return (
     <div>
       <h1>Json Place Holder</h1>
-      <h1>{users.name}</h1>
-      <p>{users.email}</p>
+      {/*   <h1>{users.name}</h1>
+      <p>{users.email}</p> */}
 
-      {/*   <h1>{posts.name}</h1>
-      <p>{posts.email}</p> */}
-      {/*  <ul>
-        {users.map((user) => {
-          <li key={user.id}> {user.name} </li>;
-        })}
-      </ul> */}
+      <ul>
+        {users.map((user) => (
+          <li key={user.id}>
+            {user.name} | {user.email}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
@@ -81,3 +99,12 @@ export default Users;
   );
 }
  */
+{
+  /* <h1>{users.name}</h1>
+      <p>{users.email}</p> */
+}
+
+{
+  /*   <h1>{posts.name}</h1>
+      <p>{posts.email}</p> */
+}
