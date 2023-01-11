69ce68a5c13015cf07da3e253b15320d77703ac6
Jahid Eljah <jahideljah@gmail.com>
2023-01-11 22:34:09 +0100

    i bootstrap

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import "bootstrap/dist/css/bootstrap.css";
 
 const baseURL = "https://jsonplaceholder.typicode.com/users";
 // const baseURL = "https://jsonplaceholder.typicode.com/users/1";
@@ -65,15 +66,36 @@ function Users() {
   return (
     <div>
       <h1>Json Place Holder</h1>
-      {/*   <h1>{users.name}</h1>
-      <p>{users.email}</p> */}
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            {user.name} || {user.email}
-          </li>
-        ))}
-      </ul>{" "}
+
+      <table className="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
