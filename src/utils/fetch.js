69ca283b7cc229d05ca5a15dc3e0e22d81f36e72
Jahid Eljah <jahideljah@gmail.com>
2023-02-09 15:20:06 +0100

    bug fix

--- src/utils/fetch.js
+++ src/utils/fetch.js
@@ -2,7 +2,7 @@ export const options = {
   method: "GET",
   url: "https://jsonplaceholder30.p.rapidapi.com/users",
   headers: {
-    "X-RapidAPI-Key": "2e69d7ab27msh2b1ed5f886aadeep12ef8cjsnd2412a7fd097",
+    "X-RapidAPI-Key": process.env.REACT_APP_API_KEY,
     "X-RapidAPI-Host": "jsonplaceholder30.p.rapidapi.com",
   },
 };
